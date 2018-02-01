62a64c8bb17c5aa61e5c4050010ff727b7b3af11
Keith Amundson <amundskm@gmail.com>
2018-02-01 00:58:30 -0600

    Added a paragraph element correctly using javascript

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ const container = document.querySelector('#container');
 const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
-
 container.appendChild(content);
+
+const para = document.createElement("p");
+para.textContent = 'hey I\'m red!';
+content.appendChild(para);
